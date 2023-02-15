b4f51c933a3a8a5555540cef0d4b08e62a77d1eb
Vladimir Viazovetskov <erhosen@gmail.com>
2023-02-15 14:06:48 +0300

    Works only for python 3.8

--- redis_index/__init__.py
+++ redis_index/__init__.py
@@ -1,5 +1,5 @@
 from redis_index.redis_index import BaseFilter, RedisFiltering, RedisIndex
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = ("BaseFilter", "RedisFiltering", "RedisIndex")
