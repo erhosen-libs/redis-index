7e9963cc4ff62f311ebad3c03e4ecc9b9f68738e
Vladimir Viazovetskov <erhosen@gmail.com>
2023-02-15 14:13:14 +0300

    Up pytest.

--- redis_index/__init__.py
+++ redis_index/__init__.py
@@ -1,5 +1,5 @@
 from redis_index.redis_index import BaseFilter, RedisFiltering, RedisIndex
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 __all__ = ("BaseFilter", "RedisFiltering", "RedisIndex")
