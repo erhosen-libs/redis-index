ff6868d8a0eddc39dcd7172d61de98199e54df9b
Vladimir Viazovetskov <erhosen@gmail.com>
2023-12-30 13:59:19 +0100

    Bump redis-index version to 0.8.0.

--- redis_index/__init__.py
+++ redis_index/__init__.py
@@ -1,5 +1,5 @@
 from redis_index.redis_index import BaseFilter, RedisFiltering, RedisIndex
 
-__version__ = "0.7.0"
+__version__ = "0.8.0"
 
 __all__ = ("BaseFilter", "RedisFiltering", "RedisIndex")
